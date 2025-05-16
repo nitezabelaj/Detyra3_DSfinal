f1bb8b780bed6f2146f0dd742d3d9145559dc8fd
nitezabelaj <nitazabelaj7@gmail.com>
2025-05-16 05:42:10 +0200

    fillimi i serverit,gjenerimi i çelesave RSA

---
+++ server.py
@@ -0,0 +1,26 @@
+import socket
+import threading
+from Crypto.Cipher import DES, PKCS1_OAEP
+from Crypto.PublicKey import RSA
+from Crypto.Util.Padding import unpad
+import base64
+
+# Gjenerojmë çelësat RSA
+key = RSA.generate(2048)
+private_key = key.export_key()
+public_key = key.publickey().export_key()
+
+# Ruajmë çelësat në skedarë (për demonstrim)
+with open('server_private.pem', 'wb') as f:
+    f.write(private_key)
+with open('server_public.pem', 'wb') as f:
+    f.write(public_key)
+
+
+def handle_client(conn):
+    try:
+        # Dërgojmë çelësin publik të serverit te klienti
+        conn.sendall(public_key)
+
+        # Marrim çelësin e DES të kriptuar me RSA
+        encrypted_des_key = conn.recv(2048)
